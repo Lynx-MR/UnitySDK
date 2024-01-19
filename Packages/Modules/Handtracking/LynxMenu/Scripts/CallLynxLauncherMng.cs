26fc3a6f2b608bc23d9cfe6d0578e56ea8ba0b12
Geoffrey MARHUENDA <g.marhuenda@slprocess.net>
2024-01-19 18:16:45 +0100

    [REFACTORING] Simplify back to launcher to a simple Application.Quit() since OpenXR Lifecycle is fixed.

--- Packages/Modules/Handtracking/LynxMenu/Scripts/CallLynxLauncherMng.cs
+++ Packages/Modules/Handtracking/LynxMenu/Scripts/CallLynxLauncherMng.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Lynx
 {
@@ -7,104 +6,7 @@ namespace Lynx
     {
         public void BackToLauncher()
         {
-            if (LynxAPI.IsAR())
-                LynxAPI.SetVR();
-
-            StartCoroutine(BackToLauncherInCoroutine());
-
-
-
-            /*
-            bool fail = false;
-
-            AndroidJavaClass up = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-            AndroidJavaObject ca = up.GetStatic<AndroidJavaObject>("currentActivity");
-            AndroidJavaObject packageManager = ca.Call<AndroidJavaObject>("getPackageManager");
-
-            AndroidJavaObject launchIntent = null;
-
-            string lynxLauncherPackageName = "com.lynx.scenehome";
-
-            try
-            {
-                launchIntent = packageManager.Call<AndroidJavaObject>("getLaunchIntentForPackage", lynxLauncherPackageName);
-            }
-            catch (System.Exception)
-            {
-                fail = true;
-            }
-
-            if (fail)
-            {  //open app in store
-                Application.OpenURL("https://lynx-r.com/");
-            }
-            else //open the app
-                ca.Call("startActivity", launchIntent);
-
-
-            Debug.Log("@@@@@@@@@@@@@@  Before finishAndRemoveTask In Lynx SDK   @@@@@@@@@@@@@@ ");
-            ca.Call("finishAndRemoveTask");
-
-            up.Dispose();
-            ca.Dispose();
-            packageManager.Dispose();
-            launchIntent.Dispose();
-
-            // is it useful ?
-            //Debug.Log("@@@@@@@@@@@@@@ Before Application quit called @@@@@@@@@@@@@@ ");
-            //Application.Quit();
-            */
-
-        }
-
-
-        IEnumerator BackToLauncherInCoroutine()
-        {
-            yield return new WaitForSecondsRealtime(0.2f);
-
-            Debug.Log("@@@@@@@@@@@@@@  BackToLauncherInCoroutine   @@@@@@@@@@@@@@ ");
-
-            bool fail = false;
-
-            AndroidJavaClass up = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-            AndroidJavaObject ca = up.GetStatic<AndroidJavaObject>("currentActivity");
-            AndroidJavaObject packageManager = ca.Call<AndroidJavaObject>("getPackageManager");
-
-            AndroidJavaObject launchIntent = null;
-
-            string lynxLauncherPackageName = "com.lynx.scenehome";
-
-            try
-            {
-                launchIntent = packageManager.Call<AndroidJavaObject>("getLaunchIntentForPackage", lynxLauncherPackageName);
-            }
-            catch (System.Exception)
-            {
-                fail = true;
-            }
-
-            if (fail)
-            {  //open app in store
-                Application.OpenURL("https://lynx-r.com/");
-            }
-            else //open the app
-                ca.Call("startActivity", launchIntent);
-
-
-            Debug.Log("@@@@@@@@@@@@@@  Before finishAndRemoveTask In Lynx SDK   @@@@@@@@@@@@@@ ");
-            ca.Call("finishAndRemoveTask");
-
-            up.Dispose();
-            ca.Dispose();
-            packageManager.Dispose();
-            launchIntent.Dispose();
-
-            // is it useful ?
-            //Debug.Log("@@@@@@@@@@@@@@ Before Application quit called @@@@@@@@@@@@@@ ");
-            //Application.Quit();
-
-
-
+            Application.Quit();
         }
     }
 }
